387ef8dee723150573cf66e5e11504dbe0a46ed6
Sakshi Saware <146549011+Sakshi-Saware@users.noreply.github.com>
2025-12-03 16:32:58 +0530

    Use environment variables for Firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDK-8y0KYUVY6cHQ6CKqTNJ16sUxx1FtJA",
-  authDomain: "bookswap2-a73d4.firebaseapp.com",
-  projectId: "bookswap2-a73d4",
-  storageBucket: "bookswap2-a73d4.firebasestorage.app",
-  messagingSenderId: "12097241729",
-  appId: "1:12097241729:web:d3a957cd7308d645c78380",
-  measurementId: "G-7L5RX4J9TN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
